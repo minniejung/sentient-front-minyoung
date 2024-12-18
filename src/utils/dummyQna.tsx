7fc35c3e70e0a09f993c6b96cbc05ae5794d2f79
minniejung <jungmignonne@gmail.com>
2024-12-18 16:21:15 +0900

    feat: add qnas

---
+++ src/utils/dummyQna.tsx
@@ -0,0 +1,95 @@
+export const DummyQna: { [key: string]: React.ReactNode } = {
+	'what is sentient?': (
+		<div className='answer-style' id='answerDiv'>
+			<p>
+				Sentient refers to the ability to experience feelings or sensations. It means being capable of sensing or
+				feeling, conscious of or responsive to the sensations of seeing, hearing. feeling, tasting, or smelling.{' '}
+			</p>
+			<b>Key Points:</b>
+			<ul>
+				<li>Sentient beings are able to feel things or sense them.</li>
+				<li>
+					The term is often used in phrases like &quot;sentient beings&quot; and &quot;sentient creatures,&quot;
+					emphasizing that things that don&apos;t have life don&apos;t have feelings.
+				</li>
+				<li>Sentient is a formal adjective that can be used in different contexts and languages.</li>
+				<li>The word has its roots in Latin, with the earliest known use dating back to the early 1600s.</li>
+			</ul>
+			<b>Examples and Usage:</b>
+			<ul>
+				<li>Man is a sentient being.</li>
+				<li>There was no sign of any sentient life or activity.</li>
+				<li>
+					Sentient is used nouns like &quot;being&quot; to describe entities that possess consciousness or the ability
+					to feel.
+				</li>
+			</ul>
+			<b>Related Concepts:</b>
+			<ul>
+				<li>
+					Sentience is an important concept in ethics, particularly in utilitarianism, as it forms a basis for
+					determining which entities deserve moral consideration.
+				</li>
+				<li>In Asian religions, the word &quot;sentience&quot; has been used to translate various concepts.</li>
+				<li>
+					In science fiction, the word &quot;sentience&quot; is often used to describe the ability of artificial
+					intelligence or other non-human entities to experience consciousness or emotions.
+				</li>
+			</ul>
+		</div>
+	),
+	'what’s the meaning of life?': (
+		<div className='answer-style' id='answerDiv'>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+			<b>List: </b>
+			<ul>
+				<li>lorem</li>
+				<li>ipsum</li>
+				<li>dolor</li>
+			</ul>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+		</div>
+	),
+	'how do you define love?': (
+		<div className='answer-style' id='answerDiv'>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+			<b>List: </b>
+			<ul>
+				<li>lorem</li>
+				<li>ipsum</li>
+				<li>dolor</li>
+			</ul>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+		</div>
+	),
+	'what’s the meaning of ai?': (
+		<div className='answer-style' id='answerDiv'>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+			<b>List: </b>
+			<ul>
+				<li>lorem</li>
+				<li>ipsum</li>
+				<li>dolor</li>
+			</ul>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi quod optio ducimus, fuga veniam dolorem ad
+				repellat consequatur exercitationem reprehenderit at odio unde ut beatae dolor sunt cum? Debitis, architecto.
+			</p>
+		</div>
+	),
+}
